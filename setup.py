96bc277de2f1d38edb43e3c5111cce75dc450b73
Jose Fonseca <jose.r.fonseca@gmail.com>
2025-12-08 20:52:23 +0000

    Bump version to 1.5.
    Quite a few fixes/enhancements accumulated since last release.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='xdot',
-    version='1.4',
+    version='1.5',
     author='Jose Fonseca',
     author_email='jose.r.fonseca@gmail.com',
     url='https://github.com/jrfonseca/xdot.py',
